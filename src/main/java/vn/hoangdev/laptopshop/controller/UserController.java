4627a4ad5eea290d516af7d8259ea8bab7120944
SAM <hoanga9k38@gmail.com>
2025-05-11 23:44:30 +0700

    Entity

--- src/main/java/vn/hoangdev/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoangdev/laptopshop/controller/UserController.java
@@ -1,8 +1,12 @@
 package vn.hoangdev.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import vn.hoangdev.laptopshop.domain.User;
 import vn.hoangdev.laptopshop.service.UserService;
 
 @Controller
@@ -14,25 +18,24 @@ public class UserController {
         this.userService = userService;
     }
     @RequestMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
         String test = this.userService.handleHello();
+        model.addAttribute("hoang",test);
+        return "hello";
+    }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("newUser",new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hoangdev) {
+        System.out.println("run here" + hoangdev);
         return "hello";
     }
 }
 
-// @RestController
-// public class UserController {
-
-//     // DI: dependency injection
-//     private UserService userService;
-    
-//     public UserController(UserService userService) {
-//         this.userService = userService;
-//     }
-
-//     @GetMapping("")
-//     public String getHomePage() {
-//         return this.userService.handleHello();
-//     }
-// }
 
